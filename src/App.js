88dd7cdc92f89b944b29e56886e5bda61714cd61
Kristen <kristenwood123@gmail.com>
2021-07-08 17:55:10 -0700

    imported styled components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
+import Navbar from "./Navbar";
 
 
 function App() {
   return (
-    <div className="App">
-    
-    </div>
+    <>
+      <Navbar />
+
+    </>
   );
 }
 
--- src/Navbar.js
+++ src/Navbar.js
@@ -2,9 +2,10 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>
-      
-    </div>
+    <nav>
+      <h2>Where in the world?</h2>
+      <p>Dark Mode</p>
+    </nav>
   )
 }
 
