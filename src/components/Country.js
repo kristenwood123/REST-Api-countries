95efd9e36ec896077d234b6e2cf83a5a96ac2887
Kristen <kristenwood123@gmail.com>
2021-07-13 16:17:00 -0700

    setup fetch tto get country names

--- src/components/Country.js
+++ src/components/Country.js
@@ -1,11 +1,43 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
-const Country = () => {
-  return (
-    <div>
-      <h1>Country</h1>
-    </div>
-  )
+function Country() {
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [countries, setCountries] = useState([]);
+
+ const url = 'https://restcountries.eu/rest/v2/all'
+
+  useEffect(() => {
+    fetch(url)
+      .then(res => res.json())
+      .then(
+        (result) => {
+          setIsLoaded(true);
+          setCountries(result);
+        },
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      )
+  }, [])
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  } else if (!isLoaded) {
+    return <div>Loading...</div>;
+  } else {
+    return (
+      <ul>
+        {countries.map(country => (
+          <li key={country.id}>
+            {country.name} {country.price}
+          </li>
+        ))}
+      </ul>
+    );
+  }
 }
 
 export default Country
+
--- src/components/data.js
+++ src/components/data.js
@@ -4,11 +4,39 @@ function data() {
   const [error, setError] = useState(null);
   const [isLoaded, setIsLoaded] = useState(false);
   const [countries, setCountries] = useState([]);
-  return (
-    <div>
-      
-    </div>
-  )
+
+ const url = 'https://restcountries.eu/rest/v2/all'
+
+  useEffect(() => {
+    fetch(url)
+      .then(res => res.json())
+      .then(
+        (result) => {
+          setIsLoaded(true);
+          setCountries(result);
+        },
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      )
+  }, [])
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  } else if (!isLoaded) {
+    return <div>Loading...</div>;
+  } else {
+    return (
+      <ul>
+        {countries.map(country => (
+          <li key={country.id}>
+            {country.name} {country.price}
+          </li>
+        ))}
+      </ul>
+    );
+  }
 }
 
 export default data
