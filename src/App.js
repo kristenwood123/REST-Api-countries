612d3b777af9706f85ac96ce0f4d4c44e9fe5079
Kristen <kristenwood123@gmail.com>
2021-07-18 16:26:42 -0700

    added details page with switch routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import { useState } from "react";
-
-import Navbar from "./components/Navbar";
-import Layout from "./components/Layout";
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import {ThemeProvider} from "styled-components";
 import { GlobalStyles } from "./components/GlobalStyles";
 import { lightTheme, darkTheme } from "./components/Theme"
+
+// App Components
+import Navbar from "./components/Navbar";
+import Layout from "./components/Layout";
+import DetailsPage from "./components/DetailsPage";
+
 
 function App() {
    const [theme, setTheme] = useState('light');
@@ -15,11 +18,12 @@ function App() {
 }
   return (
     <ThemeProvider theme={theme === 'light' ? lightTheme : darkTheme}>
-          <Navbar themeToggler={themeToggler} />
-          <GlobalStyles />
-          <Route path='/'>
-            <Layout />
-          </Route>
+      <Navbar themeToggler={themeToggler} />
+      <GlobalStyles />
+
+        <Switch>
+          <Route exact path='/' component={Layout}/>
+          <Route path='/details' component={DetailsPage}/>          </Switch>
     </ThemeProvider>
   );
 }
